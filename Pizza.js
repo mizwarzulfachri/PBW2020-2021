9bfe9d1ffbf6e43621099a72bbe47f50383d3d3d
mizwarzulfachri <76586788+mizwarzulfachri@users.noreply.github.com>
2021-03-13 18:53:14 +0700

    Add files via upload

---
+++ Pizza.js
@@ -0,0 +1,6 @@
+var name = "Mizwar Zulfachri";
+var address = "Banda Aceh";
+
+document.getElementById("nama").innerHTML = "Nama Saya Mizwar dan kopi Java sama Pizza enak ya. <br/>Atau tidak?";
+document.getElementById('name').innerHTML = name;
+document.getElementById('address').innerHTML = address;
